957259d085c5697cfde6a8accad37702d4b1ebfb
RobertxDessinacap <roberto.yanez15@inacapmail.cl>
2025-08-27 01:49:17 -0400

    Conexion a la base de datos solo INDEXUS

--- Django/JR/views.py
+++ Django/JR/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Contactos
 
 
 def test(request):
     return render(request, 'hola.html')
 
 def pagina(request):
-    return render(request, 'IndexUS.html')
+    if request.method == 'POST':
+        Contactos.objects.create(nombre=request.POST['nombre'], 
+                                apellido=request.POST['apellido'], 
+                                email=request.POST['email'], 
+                                motivo=request.POST['motivo'])
+        return render(request, 'IndexUS.html')
+    else:    
+        return render(request, 'IndexUS.html')
 
 #Pagina del Juan
 def PaginaJuan(request):
