2dfceddd18c290e2eb13b72ff56082f6154b1c55
Juan <jsanhueg2002@gmail.com>
2025-08-26 23:52:26 -0400

    Cambios en el css

--- Django/JR/static/scriptNosotros.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("botonJextra").addEventListener("click", function() {
-    const extraInfo = document.getElementById("extra");
-    extraInfo.classList.toggle("info-visible");
-    extraInfo.classList.toggle("info-oculta");
-});
