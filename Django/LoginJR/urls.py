78e97f773da59acf8d7d279fa7b26b3507dc3a7d
RobertxDessinacap <roberto.yanez15@inacapmail.cl>
2025-09-10 21:50:10 -0300

    Implementacion de visualDB

--- Django/LoginJR/urls.py
+++ Django/LoginJR/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import LoginJR
+from .views import LoginJR, testlogin, mostrardatos
 
 urlpatterns = [
-    path('', LoginJR)
+    path('', LoginJR),
+    path('/test/', testlogin),
+    path('/visual/', mostrardatos)
 ]
--- Django/LoginJR/views.py
+++ Django/LoginJR/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from hashlib import sha256
+
 
 # Create your views here.
 def LoginJR(request):
     return render(request, 'login.html')
+
+
+def testlogin(request):
+
+    return render(request, 'testlogin.html', {
+        'form': UserCreationForm
+    })
+
+def mostrardatos(request):
+    return render(request, 'visualDB.html')
